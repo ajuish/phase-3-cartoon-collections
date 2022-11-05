5feab24ba367ffeaaf9a486812d4b24805ed5aa7
Alex Ju <aju217@gmail.com>
2022-11-05 14:54:23 -0400

    commit tree 4807f30a2fe20e1f13fc5504f2f00f7f5c52d702

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)
   # Your code here
+  elements.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(options)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  options.find do |option|
+    cheese_types.include?(option)
+  end
 end
